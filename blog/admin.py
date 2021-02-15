9a140a4c1c7f155cb6da8a594e037ff6d213dad2
vladetec <vladetec@gmail.com>
2021-02-15 19:57:24 -0300

    Created Models and Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 from .models import Post
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug",'sub_title', 'full_name', "author", "created", "updated", 'active' )
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields  = ['title', 'sub_title']
+    #fields = ('title', 'sub_title')
+    #def get_queryset(self, request):
+    #    return Post.objects.filter(deleted=False)
+
+#admin.site.register(Post, PostAdmin)
+#admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,50 @@
+from django.contrib.auth.models import User
 from django.db import models
+import uuid
+from stdimage.models import StdImageField
+from django.urls import reverse
+
+
+def get_file_path(_instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return filename
+
+"""
+class Base(models.Model):
+    created = models.DateTimeField('Criação',auto_now_add=True)
+    updated = models.DateTimeField('Atualização',auto_now=True)
+    active = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+"""
+
+
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    sub_title = models.CharField(max_length=200)
-    content = models.TextField()
+    title = models.CharField(max_length=60)
+    slug = models.SlugField(max_length=60, unique=True, null=True, blank=True,)
+    sub_title = models.CharField(max_length=60)
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True,)
+    imagem = StdImageField('Imagem', upload_to=get_file_path, null=True,
+     blank=True, variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True,)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField('Ativo?', default=True) 
+    
+    class Meta:
+        verbose_name = 'Titulo'
+        verbose_name_plural = 'Titulos'
+        ordering = ("-created",)
+
+    def __str__(self):
+        return self.title
+
+    def full_name(self):
+        return self.title + self.sub_title    
+
+    def get_absolute_url(self):
+        return reverse("blog:detail", kwargs={"slug": self.slug})
+
