29261a96a79886853de533388badd2002720d74b
vladetec <vladetec@gmail.com>
2021-02-15 16:06:50 -0300

    Frist Run

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def home(request):
+    
+    lista = [
+        'Welcome-To-The-Django',
+        'Passaport-Dev-Sênior',
+        'Raio-X-Regex',
+        'Raio-X-POO',
+        'Raio-x-TDD'
+        ]
+    lista_post = Post.objects.all()    
+    data = {'Nome': 'Vlademir', 'Ano': 2021, 'Cursos': lista, 'Post': lista_post}
+    return render(request, 'index.html', data)  
